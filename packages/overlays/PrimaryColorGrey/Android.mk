b585ac70f25da5028178603da700f5dfe67d3569
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-02 15:53:31 +0000

    Color fixup

--- packages/overlays/PrimaryColorGrey/Android.mk
+++ packages/overlays/PrimaryColorGrey/Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := PrimaryColorOmniBlack
+LOCAL_RRO_THEME := PrimaryColorGrey
 
 LOCAL_PRODUCT_MODULE := true
 
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := PrimaryColorOmniBlackOverlay
+LOCAL_PACKAGE_NAME := PrimaryColorGreyOverlay
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
