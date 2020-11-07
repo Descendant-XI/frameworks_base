0d1d7cf94ab8031f03a775f3770551e46117de65
ezio84 <brabus84@gmail.com>
2020-11-08 00:06:52 +0100

    ScreenOFF volume skiptrack (with remote Spotify support)
    and other players that allow music controls for remote devices (PC, PS4)
    through the media notification

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4902,6 +4902,12 @@ public final class Settings {
          */
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
 
+        /** Whether to skip music track with volume rocker 
+        /**
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
