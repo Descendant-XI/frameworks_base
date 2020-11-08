1a463ea37f828a88cfd1f09250a07d00b6a2b024
ezio84 <brabus84@gmail.com>
2020-11-08 11:03:19 +0100

    Ambient music ticker: double tap on track title to skip track
    TODO: KeyguardSliceProvider ListBuilder api allows to set an
    action (SliceAction) to the slices, that triggers a provided pendingintent.
    So we could add it then get the action in KeyguardSliceView with header.getPrimaryAction
    and record it in mClickActions to load and trigger it later in the
    onClick
    
    ----
    
    Ambient music ticker AoD: make it compatibile with double tap sensor
    
    also don't wake up on skip track events when pulsing Ambient
    
    TLDR: some devices like the Pixel2xl have a specific double tap sensor for AoD
    that handles the touches in this screen state and nothing is dispatched
    to StatusBarWindowView, and it's enabled by the double tap to wake
    option available in Settings - System - Gestures.
    
    We can use the related onDoubleTap callback that Google already added
    on purpose to manage double taps  on the AmbientContainer (Now Playing on
    Android P) when AoD is showing, to trigger the skip track event we need.
    
    ------
    
    Disable ticker DT2SkipTrack and wake to Ambient on Soli devices
    
    Soli has its own implementation for both things
    
    Change-Id: Idb5bab48718c8668bc3868e34ac0e844a06d77ef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1898,6 +1898,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mDozeServiceHost.isPulsing();
     }
 
+    public boolean isDoubleTapOnMusicTicker(float screenX, float screenY) {
+         if (mDozeServiceHost != null) {
+            return mDozeServiceHost.isDoubleTapOnMusicTicker(screenX, screenY);
+        }
+        return false;
+    }
+
     public boolean hideStatusBarIconsWhenExpanded() {
         return mNotificationPanelViewController.hideStatusBarIconsWhenExpanded();
     }
