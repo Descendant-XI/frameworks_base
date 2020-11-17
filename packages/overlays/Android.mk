6efdccb13758fc0a0c75e5b195a905bf5a851b62
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-17 01:56:25 +0000

    Icons: build heartshaped

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -69,6 +69,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
+	IconShapeHeartOverlay \
 	IconShapePebbleOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
