bf0570568ec64e9070a31fae29ce65c21ee437bc
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-25 18:41:52 +0000

    Fix Metrics

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -111,7 +111,7 @@ public class HeadsUpTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.POTATO;
+        return MetricsEvent.DESCENDANT_METRICS;
     }
 
     @Override
